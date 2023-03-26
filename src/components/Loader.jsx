52f527e228862445d1e7cb8a53e1737b7e8631cd
Natan Journo <natanjourno@MBP-de-Natan.lan>
2023-03-26 13:40:17 +0200

    3️⃣ Step 3 : Create Hero section with animated component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei";
 
-const Loader = () => {
+const CanvasLoader = () => { // CanvasLoader displays a percentage of progress while loading 3D resources.
+  const { progress } = useProgress(); // useProgress to get the progress object and extract the progress value.
   return (
-    <div>Loader</div>
-  )
-}
+    <Html
+      as='div'
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className='canvas-loader'></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      > 
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
 
-export default Loader
+/* The result of the toFixed(2) method is inserted into the JSX code between braces 
+to display directly on the screen, followed by the percentage symbol (%). */
+
+export default CanvasLoader;
